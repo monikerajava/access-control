19d8e2e700bb7098b72ffc32ca5f83e016b30c98
MONIKERAJAVA <monica.dominog@gmail.com>
2021-09-04 13:36:31 -0700

    Publicacao do projeto

---
+++ src/main/java/com/dio/accesscontrol/model/Localidade.java
@@ -0,0 +1,24 @@
+package com.dio.accesscontrol.model;
+
+import lombok.*;
+import org.hibernate.annotations.ManyToAny;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class Localidade {
+    @Id
+    private long id;
+    @ManyToOne
+    private NivelAcesso nivelAcesso;
+    private String descricao;
+
+}
---
+++ src/main/java/com/dio/accesscontrol/model/Ocorrencia.java
@@ -0,0 +1,26 @@
+package com.dio.accesscontrol.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class Ocorrencia {
+    @Id
+    private long id;
+    private String nome;
+    private String descricao;
+
+}
